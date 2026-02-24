46525fd6890c155176f1bcc7ddfc817fd3c84494
abme-la <232544417+abme-la@users.noreply.github.com>
2026-02-24 15:20:41 +0100

    Changed tests to also use enums.

--- src/test/java/hu/aut/bme/springlab/CountryMapperTest.java
+++ src/test/java/hu/aut/bme/springlab/CountryMapperTest.java
@@ -5,27 +5,28 @@ import org.junit.jupiter.api.Test;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static hu.aut.bme.springlab.CountryData.Continent.*;
 
- public class CountryMapperTest {
+public class CountryMapperTest {
         @Test
         public void testWithRealCountryData() {
-            var input = "Hungary, Europe, 12321, 12312321";
-            var countryData = new CountryMapper().mapToCountryData(input);
-            assertEquals("Hungary", countryData.country());
-            assertEquals("Europe", countryData.region());
-            assertEquals(12321, countryData.population());
-            assertEquals(12312321, countryData.area());
-            assertEquals(Optional.of("Europe"), countryData.continent());
+                var input = "Hungary, Europe, 12321, 12312321";
+                var countryData = new CountryMapper().mapToCountryData(input);
+                assertEquals("Hungary", countryData.country());
+                assertEquals("Europe", countryData.region());
+                assertEquals(12321, countryData.population());
+                assertEquals(12312321, countryData.area());
+                assertEquals(Optional.of(EUROPE), countryData.continent());
         }
 
-     @Test
-     public void testWithBogusCountryData() {
-         var input = "Neverland, Nowhere, 50, 0";
-         var countryData = new CountryMapper().mapToCountryData(input);
-         assertEquals("Neverland", countryData.country());
-         assertEquals("Nowhere", countryData.region());
-         assertEquals(50, countryData.population());
-         assertEquals(0, countryData.area());
-         assertEquals(Optional.empty(), countryData.continent());
-     }
-    }
+        @Test
+        public void testWithBogusCountryData() {
+                var input = "Neverland, Nowhere, 50, 0";
+                var countryData = new CountryMapper().mapToCountryData(input);
+                assertEquals("Neverland", countryData.country());
+                assertEquals("Nowhere", countryData.region());
+                assertEquals(50, countryData.population());
+                assertEquals(0, countryData.area());
+                assertEquals(Optional.empty(), countryData.continent());
+        }
+}
