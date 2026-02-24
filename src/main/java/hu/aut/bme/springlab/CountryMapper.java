075a2049b7c6bac7ac60a4b17855c963fb492943
abme-la <232544417+abme-la@users.noreply.github.com>
2026-02-24 13:19:59 +0100

    Revert "Introduced error."
    This reverts commit e147631037393f110742e0833316be915de22e7d.

--- src/main/java/hu/aut/bme/springlab/CountryMapper.java
+++ src/main/java/hu/aut/bme/springlab/CountryMapper.java
@@ -3,6 +3,6 @@ package hu.aut.bme.springlab;
 public class CountryMapper {
     public CountryData mapToCountryData(String line) {
         var columns = line.split(",");
-        return new CountryData(columns[0].trim(), columns[0].trim(), Long.parseLong(columns[2].trim()), Long.parseLong(columns[3].trim()));
+        return new CountryData(columns[0].trim(), columns[1].trim(), Long.parseLong(columns[2].trim()), Long.parseLong(columns[3].trim()));
     }
 }
