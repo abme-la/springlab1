9211d20a4e1422904fd701832752ea75ecb1742a
abme-la <232544417+abme-la@users.noreply.github.com>
2026-02-24 14:42:16 +0100

    Moved continets to enums.

--- src/main/java/hu/aut/bme/springlab/CountryData.java
+++ src/main/java/hu/aut/bme/springlab/CountryData.java
@@ -2,5 +2,8 @@ package hu.aut.bme.springlab;
 
 import java.util.Optional;
 
-public record CountryData(String country, String region, long population, long area, Optional<String> continent) {
+public record CountryData(String country, String region, long population, long area, Optional<Continent> continent) {
+        public static enum Continent {
+                EUROPE, AFRICA, ASIA, OCEANIA, AMERICA, AUSTRALIA, ANTARCTICA
+        }
 }
--- src/main/java/hu/aut/bme/springlab/CountryMapper.java
+++ src/main/java/hu/aut/bme/springlab/CountryMapper.java
@@ -2,22 +2,26 @@ package hu.aut.bme.springlab;
 
 import java.util.Optional;
 
-public class CountryMapper {
-    public CountryData mapToCountryData(String line) {
-        var columns = line.split(",");
-        return new CountryData(columns[0].trim(), columns[1].trim(), Long.parseLong(columns[2].trim()), Long.parseLong(columns[3].trim()), findContinent(columns[1]));
-    }
+import hu.aut.bme.springlab.CountryData.Continent;
+import static hu.aut.bme.springlab.CountryData.Continent.*;
 
-    private Optional<String> findContinent(String region) {
-        return switch (region) {
-            case String s when s.toLowerCase().contains("europe")-> Optional.of("Europe");
-            case String s when s.toLowerCase().contains("africa")-> Optional.of("Africa");
-            case String s when s.toLowerCase().contains("near east")-> Optional.of("Asia");
-            case String s when s.toLowerCase().contains("oceania")-> Optional.of("Oceania");
-            case String s when s.toLowerCase().contains("amer")-> Optional.of("America");
-            case String s when s.toLowerCase().contains("baltics")-> Optional.of("Europe");
-            case String s when s.toLowerCase().contains("c.w.")-> Optional.of("Asia");
-            default -> Optional.empty();
-        };
-    }
+public class CountryMapper {
+        public CountryData mapToCountryData(String line) {
+                var columns = line.split(",");
+                return new CountryData(columns[0].trim(), columns[1].trim(), Long.parseLong(columns[2].trim()),
+                                Long.parseLong(columns[3].trim()), findContinent(columns[1]));
+        }
+
+        private Optional<Continent> findContinent(String region) {
+                return switch (region) {
+                        case String s when s.toLowerCase().contains("europe") -> Optional.of(EUROPE);
+                        case String s when s.toLowerCase().contains("africa") -> Optional.of(AFRICA);
+                        case String s when s.toLowerCase().contains("near east") -> Optional.of(ASIA);
+                        case String s when s.toLowerCase().contains("oceania") -> Optional.of(OCEANIA);
+                        case String s when s.toLowerCase().contains("amer") -> Optional.of(AMERICA);
+                        case String s when s.toLowerCase().contains("baltics") -> Optional.of(EUROPE);
+                        case String s when s.toLowerCase().contains("c.w.") -> Optional.of(ASIA);
+                        default -> Optional.empty();
+                };
+        }
 }
